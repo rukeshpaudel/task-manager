4480df702d2fcbabd2d46534d6b71d0ac6a1cc19
Rukesh Paudel <rukeshpaudel@gmail.com>
2022-07-01 21:46:24 +0545

    Added functionalities

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,7 +3,11 @@ const Task = require('../models/Task')
 const getAlltasks=async(req,res)=>{
     try {
         const tasks= await Task.find({})
+
+            
         res.status(200).json({tasks})
+     //   other options: ----> res.status(200).json({status:"success",data:{tasks,nbHits: tasks.length} })
+    
     } catch (error) {
         res.status(500).json({msg:error})
         
